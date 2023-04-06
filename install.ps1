5dfb6d2b0243b1b80a4faea3b8b3508c17595dbe
William Gawlik <william.gawlik@outlook.com>
2023-04-06 08:53:59 -0500

    Fixed install script

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
-$ac_dir = "E:\Games\Steam\steamapps\common\assettocorsa\apps\lua\mps_preset"
+$ac_dir = "E:\Games\Steam\steamapps\common\assettocorsa\apps\lua\"
 
 Copy-Item "$PSScriptRoot\mps_preset\" -Destination $ac_dir -Recurse -Force
